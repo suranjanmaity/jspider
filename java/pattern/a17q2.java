2267ba10c9a34606ed1ebfef4f736cfbe79519ab
suranjanmaity <suranjanmaity4@gmail.com>
2022-08-22 23:52:21 +0530

    assignment 17

---
+++ java/pattern/a17q2.java
@@ -0,0 +1,16 @@
+class Pattern2
+{
+    public static void main (String[]args)
+    {
+        char ch = 'a';
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
